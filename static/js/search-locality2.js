3ba4cc262d006c8a2904198b830186a2ed20adfb
Williams, Dartanion De Angelo <dwilliams@bootcampspot.com>
2020-03-31 10:02:34 -0400

    Updating code

--- static/js/search-locality2.js
+++ static/js/search-locality2.js
@@ -253,6 +253,7 @@ function initExportTable()
   return exportTable;
 }
 
+/*
 function fnExcelReport()
 {
 
@@ -288,10 +289,53 @@ function fnExcelReport()
         sa=reportWindow.document.execCommand("SaveAs",true,"COVID-VA-Export.xlsx");
     }
     else                 //other browser not tested on IE 11
-        sa = window.open('data:application/vnd.ms-excel,' + encodeURIComponent(tab_text));
+        sa = window.open('data:application/vnd.ms-excel;base64,' + encodeURIComponent(tab_text));
 
     return (sa);
 }
+*/
+
+
+function exportTableToExcel(tableID, filename = 'covid-response-va'){
+    var downloadLink;
+    var dataType = 'application/vnd.ms-excel';
+    //var tableSelect = document.getElementById(tableID);
+    var tableSelect = document.getElementById('exportTable');
+
+    var tableSelect = generateFullExport(globalData);
+
+
+    var tableHTML = tableSelect.outerHTML.replace(/ /g, '%20');
+
+
+    // Specify file name
+    filename = filename?filename+'.xlsx':'excel_data.xlsx';
+
+    // Create download link element
+    downloadLink = document.createElement("a");
+
+    document.body.appendChild(downloadLink);
+
+    if(navigator.msSaveOrOpenBlob){
+        var blob = new Blob(['\ufeff', tableHTML], {
+            type: dataType
+        });
+        navigator.msSaveOrOpenBlob( blob, filename);
+    }else{
+        // Create a link to the file
+        downloadLink.href = 'data:' + dataType + ', ' + tableHTML;
+
+        // Setting the file name
+        downloadLink.download = filename;
+
+        //triggering the function
+        downloadLink.click();
+    }
+}
+
+
+
+
 
 function generateFullExport(data) {
 
